5c22188f758a125c9a4dfde28bf903d78028188a
JerryAhn <jb7959@naver.com>
2015-04-21 07:31:51 +0000

    20150421

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :carts
 
-  get 'store/index'
+  get 'store/index' # 
   resources :products
   root to: 'store#index', as: 'store'
   # The priority is based upon order of creation: first created -> highest priority.
